b3438fae985d8c728dcab0b3ed6c8f0d381e5e5e
JayShen <102373424+JayShen17@users.noreply.github.com>
2022-03-26 21:47:56 +0800

    Update ABC.java

--- src/main/java/com/atguigu/util/ABC.java
+++ src/main/java/com/atguigu/util/ABC.java
@@ -3,8 +3,8 @@ package com.atguigu.util;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("11111master");
-        System.out.println("11111dev");
-        System.out.println("11111dev");
+        System.out.println("11111dev5555");
+        System.out.println("11111dev6666666");
         System.out.println("11111dev");
         System.out.println("11111dev");
         System.out.println("11111dev2222222");
